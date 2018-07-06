ed73a0df426e4cbb3419a30f4889d0fff07e84ef
emarx <elliot@havenmoney.com>
2018-07-06 15:14:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="anomaly_detection",
     packages=["anomaly_detection"],
     version="0.0.2",
+    install_requires=["scipy", "statsmodels"],
     description="A python implementation of https://github.com/twitter/AnomalyDetection",
 )
